7527b93f12b122ffc4f818393ff8deca41ca2910
Woulfe <58498527+pejovskitoni@users.noreply.github.com>
2020-03-24 14:04:24 -0600

    modified StringPractice class

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -4,5 +4,7 @@ public class StringPractice {
 
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        String str= "Java is Fun!";
+        System.out.println(str);
     }
 }
